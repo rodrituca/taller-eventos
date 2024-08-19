c165cdabb716d9add4ec7ff6cd63aca76fcedeb5
rodrituca <ezecapybara@gmail.com>
2024-08-19 14:57:22 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 divSaludo.addEventListener("click", function() {
 alert("Hola! Soy el Div") });
+
+btnSaludo.addEventListener("click", function(event) {
+    alert("Hola! Soy el Botón");
+    event.stopPropagation();
+});
+
 });
